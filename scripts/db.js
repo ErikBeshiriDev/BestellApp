8ea0196a73b006b63822c27e682b947f8ab20c01
Erik Beshiri <148581637+ErikBeshiriDev@users.noreply.github.com>
2026-04-28 00:56:37 +0200

    Adjustments in the db.js data, change of underscores to normal centred lines

--- scripts/db.js
+++ scripts/db.js
@@ -1,71 +1,71 @@
 let menu = [
     {
-        "menu_title": "Burger & Sandwiches",
-        "menu_component_1": {
-            "meal_name_1": "Veggie mushroom black burger",
-            "menu_ingredients_1": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-            "menu_price_1": "16,90€",
+        "menu-title": "Burger & Sandwiches",
+        "menu-component-1": {
+            "meal-name-1": "Veggie mushroom black burger",
+            "menu-ingredients-1": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "menu-price-1": "16,90€",
         },
-        "menu_component_2": {
-            "meal_name_2": "All meat burger",
-            "menu_ingredients_2": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-            "menu_price_2": "15,90€",
+        "menu-component-2": {
+            "meal-name-2": "All meat burger",
+            "menu-ingredients-2": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+            "menu-price-2": "15,90€",
         },
-        "menu_component_3": {
-            "meal_name_3": "Beef red burger",
-            "menu_ingredients_3": "Beef, Cheese, Tomatoes, Lettuce, Onion",
-            "menu_price_3": "14,90€",
+        "menu-component-3": {
+            "meal-name-3": "Beef red burger",
+            "menu-ingredients-3": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "menu-price-3": "14,90€",
         },
-        "menu_component_4": {
-            "meal_name_4": "Big chicken burger",
-            "menu_ingredients_4": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bel pepper",
-            "menu_price_4": "15,90€",
+        "menu-component-4": {
+            "meal-name-4": "Big chicken burger",
+            "menu-ingredients-4": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bel pepper",
+            "menu-price-4": "15,90€",
         }
     },
     {
-        "menu_title": "Pizza (30cm)",
-        "menu_component_1": {
-            "meal_name_1": "Pizza Margherita",
-            "menu_ingredients_1": "Tomato slices, Mozzarella, Chorizo",
-            "menu_price_1": "11,90€",
+        "menu-title": "Pizza (30cm)",
+        "menu-component-1": {
+            "meal-name-1": "Pizza Margherita",
+            "menu-ingredients-1": "Tomato slices, Mozzarella, Chorizo",
+            "menu-price-1": "11,90€",
         },
-        "menu_component_2": {
-            "meal_name_2": "Pizza Chorizo",
-            "menu_ingredients_2": "Tomato slices, Mozzarella, Chorizo",
-            "menu_price_2": "13,90€",
+        "menu-component-2": {
+            "meal-name-2": "Pizza Chorizo",
+            "menu-ingredients-2": "Tomato slices, Mozzarella, Chorizo",
+            "menu-price-2": "13,90€",
         },
-        "menu_component_3": {
-            "meal_name_3": "Funghi",
-            "menu_ingredients_3": "Red onion, Olives, Button Mushroooms, Mozzarella",
-            "menu_price_3": "12,90€",
+        "menu-component-3": {
+            "meal-name-3": "Funghi",
+            "menu-ingredients-3": "Red onion, Olives, Button Mushroooms, Mozzarella",
+            "menu-price-3": "12,90€",
         },
-        "menu_component_4": {
-            "meal_name_4": "Quattro Formaggi with Chicken",
-            "menu_ingredients_4": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-            "menu_price_4": "15,90€",
+        "menu-component-4": {
+            "meal-name-4": "Quattro Formaggi with Chicken",
+            "menu-ingredients-4": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "menu-price-4": "15,90€",
         }
     },
     {
-        "menu_title": "Salad",
-        "menu_component_1": {
-            "meal_name_1": "Warm beef arugula salad",
-            "menu_ingredients_1": "Beef, Arugula, Field salad, Greek feta, Cherytomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-            "menu_price_1": "16,90€",
+        "menu-title": "Salad",
+        "menu-component-1": {
+            "meal-name-1": "Warm beef arugula salad",
+            "menu-ingredients-1": "Beef, Arugula, Field salad, Greek feta, Cherytomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+            "menu-price-1": "16,90€",
         },
-        "menu_component_2": {
-            "meal_name_2": "Mini green Salad",
-            "menu_ingredients_2": "Green salad, Cucumber, Carrots, Parsley, Radishes",
-            "menu_price_2": "7,90€",
+        "menu-component-2": {
+            "meal-name-2": "Mini green Salad",
+            "menu-ingredients-2": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+            "menu-price-2": "7,90€",
         },
-        "menu_component_3": {
-            "meal_name_3": "Green Salad with sea food",
-            "menu_ingredients_3": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-            "menu_price_3": "16,90€",
+        "menu-component-3": {
+            "meal-name-3": "Green Salad with sea food",
+            "menu-ingredients-3": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+            "menu-price-3": "16,90€",
         },
-        "menu_component_4": {
-            "meal_name_4": "Vegan green salad with tofu",
-            "menu_ingredients_4": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-            "menu_price_4": "14,90€",
+        "menu-component-4": {
+            "meal-name-4": "Vegan green salad with tofu",
+            "menu-ingredients-4": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "menu-price-4": "14,90€",
         }
     }
 ]
