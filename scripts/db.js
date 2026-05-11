3da858169ab50a81c93e9a351b0991c552d9090b
Erik Beshiri <148581637+ErikBeshiriDev@users.noreply.github.com>
2026-05-11 10:28:26 +0200

    feat: update meal descriptions and prices in menu data and display functions

--- scripts/db.js
+++ scripts/db.js
@@ -3,69 +3,69 @@ let menu = [
         "menu-title": "Burger & Sandwiches",
         "menu-component-1": {
             "meal-name-1": "Veggie mushroom black burger",
-            "menu-ingredients-1": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-            "menu-price-1": "16,90€",
+            "meal-ingredients-1": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "meal-price-1": "16,90€",
         },
         "menu-component-2": {
             "meal-name-2": "All meat burger",
-            "menu-ingredients-2": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-            "menu-price-2": "15,90€",
+            "meal-ingredients-2": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+            "meal-price-2": "15,90€",
         },
         "menu-component-3": {
             "meal-name-3": "Beef red burger",
-            "menu-ingredients-3": "Beef, Cheese, Tomatoes, Lettuce, Onion",
-            "menu-price-3": "14,90€",
+            "meal-ingredients-3": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "meal-price-3": "14,90€",
         },
         "menu-component-4": {
             "meal-name-4": "Big chicken burger",
-            "menu-ingredients-4": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bel pepper",
-            "menu-price-4": "15,90€",
+            "meal-ingredients-4": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bel pepper",
+            "meal-price-4": "15,90€",
         }
     },
     {
         "menu-title": "Pizza (30cm)",
         "menu-component-1": {
             "meal-name-1": "Pizza Margherita",
-            "menu-ingredients-1": "Tomato slices, Mozzarella, Chorizo",
-            "menu-price-1": "11,90€",
+            "meal-ingredients-1": "Tomato slices, Mozzarella, Chorizo",
+            "meal-price-1": "11,90€",
         },
         "menu-component-2": {
             "meal-name-2": "Pizza Chorizo",
-            "menu-ingredients-2": "Tomato slices, Mozzarella, Chorizo",
-            "menu-price-2": "13,90€",
+            "meal-ingredients-2": "Tomato slices, Mozzarella, Chorizo",
+            "meal-price-2": "13,90€",
         },
         "menu-component-3": {
             "meal-name-3": "Funghi",
-            "menu-ingredients-3": "Red onion, Olives, Button Mushroooms, Mozzarella",
-            "menu-price-3": "12,90€",
+            "meal-ingredients-3": "Red onion, Olives, Button Mushroooms, Mozzarella",
+            "meal-price-3": "12,90€",
         },
         "menu-component-4": {
             "meal-name-4": "Quattro Formaggi with Chicken",
-            "menu-ingredients-4": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-            "menu-price-4": "15,90€",
+            "meal-ingredients-4": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "meal-price-4": "15,90€",
         }
     },
     {
         "menu-title": "Salad",
         "menu-component-1": {
             "meal-name-1": "Warm beef arugula salad",
-            "menu-ingredients-1": "Beef, Arugula, Field salad, Greek feta, Cherytomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-            "menu-price-1": "16,90€",
+            "meal-ingredients-1": "Beef, Arugula, Field salad, Greek feta, Cherytomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+            "meal-price-1": "16,90€",
         },
         "menu-component-2": {
             "meal-name-2": "Mini green Salad",
-            "menu-ingredients-2": "Green salad, Cucumber, Carrots, Parsley, Radishes",
-            "menu-price-2": "7,90€",
+            "meal-ingredients-2": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+            "meal-price-2": "7,90€",
         },
         "menu-component-3": {
             "meal-name-3": "Green Salad with sea food",
-            "menu-ingredients-3": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-            "menu-price-3": "16,90€",
+            "meal-ingredients-3": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+            "meal-price-3": "16,90€",
         },
         "menu-component-4": {
             "meal-name-4": "Vegan green salad with tofu",
-            "menu-ingredients-4": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-            "menu-price-4": "14,90€",
+            "meal-ingredients-4": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "meal-price-4": "14,90€",
         }
     }
 ]
--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ function init() {
 function showInformationsOnTheEntireMenu() {
     showAllMenuNames();
     showAllMenuPrices();
+    showAllMenuMealDescriptions();
 };
 
 function showAllMenuNames() {
@@ -15,8 +16,15 @@ function showAllMenuNames() {
 };
 
 function showAllMenuPrices() {
-    document.getElementById('meal-category-1-price-1').innerHTML = menu[0]['menu-component-1']['menu-price-1'];
-    document.getElementById('meal-category-1-price-2').innerHTML = menu[0]['menu-component-2']['menu-price-2'];
-    document.getElementById('meal-category-1-price-3').innerHTML = menu[0]['menu-component-3']['menu-price-3'];
-    document.getElementById('meal-category-1-price-4').innerHTML = menu[0]['menu-component-4']['menu-price-4'];
+    document.getElementById('meal-category-1-price-1').innerHTML = menu[0]['menu-component-1']['meal-price-1'];
+    document.getElementById('meal-category-1-price-2').innerHTML = menu[0]['menu-component-2']['meal-price-2'];
+    document.getElementById('meal-category-1-price-3').innerHTML = menu[0]['menu-component-3']['meal-price-3'];
+    document.getElementById('meal-category-1-price-4').innerHTML = menu[0]['menu-component-4']['meal-price-4'];
+};
+
+function showAllMenuMealDescriptions() {
+    document.getElementById('meal-category-1-meal-description-1').innerHTML = menu[0]['menu-component-1']['meal-ingredients-1'];
+    document.getElementById('meal-category-1-meal-description-2').innerHTML = menu[0]['menu-component-2']['meal-ingredients-2'];
+    document.getElementById('meal-category-1-meal-description-3').innerHTML = menu[0]['menu-component-3']['meal-ingredients-3'];
+    document.getElementById('meal-category-1-meal-description-4').innerHTML = menu[0]['menu-component-4']['meal-ingredients-4'];
 };
