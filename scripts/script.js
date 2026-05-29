bf47cebc2224466d103b52ebae70e07a00144e99
Erik Beshiri <148581637+ErikBeshiriDev@users.noreply.github.com>
2026-05-29 15:34:53 +0200

    refactor: rename function and improve basket item addition logic

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,13 @@ function init() {
 function showInformationsOnTheEntireMenu() {
     for (let a = 0; a < menu.length; a++) {
         for (let b = 1; b <= 4; b++) {
-            const context = allConstLines(a, b);
+            const context = allFirstConstLines(a, b);
             menuInsertingIntoTheIDs(a, b, context);
         }
     }
 };
 
-function allConstLines(a, b) {
+function allFirstConstLines(a, b) {
     const htmlNameId = `meal-category-${a + 1}-name-${b}`;
     const htmlPriceId = `meal-category-${a + 1}-price-${b}`;
     const htmlDescriptionId = `meal-category-${a + 1}-meal-description-${b}`;
@@ -39,13 +39,16 @@ function menuInsertingIntoTheIDs(a, b, context) {
 };
 
 function addToBasket(element) {
-    const addedIntoTheBasket = 'Added 1';
-    element.innerHTML = addedIntoTheBasket;
+    const buttonNumber = element.id.split('-').pop();
+    let count = Number(element.dataset.count || 0);
+    count += 1;
+    element.dataset.count = count;
+    element.innerHTML = `Added ${count}`;
     element.classList.add('orange_text');
-    newMenuBasketBox();
+    newMenuBasketBox(buttonNumber);
 };
 
-function newMenuBasketBox() {
+function newMenuBasketBox(buttonNumber) {
     let basketBox = document.getElementById('the-basket-list');
     basketBox.innerHTML += `
         <div class="item_in_the_basket" id="meal-basket-box">
